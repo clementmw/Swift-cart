f06b05469cc0f89db5e849e0acebbced7dac6d94
clementmwamba <clementmwamba23@gmail.com>
2023-12-09 01:23:06 +0300

    installed tailwind

---
+++ pr/src/App.js
@@ -0,0 +1,16 @@
+import {  Routes,Route } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>project</h1>
+
+    <Routes>
+      <Route></Route>
+    </Routes>
+    </div>
+  );
+}
+
+export default App;
