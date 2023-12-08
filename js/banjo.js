b55ebbafe16aafc9c4de7aeeca3678c4bb930612
clementmwamba <clementmwamba23@gmail.com>
2023-12-08 18:43:38 +0300

    6

---
+++ js/banjo.js
@@ -0,0 +1,5 @@
+const areYouPlayingBanjo = (name)=>{
+     return name[0] === "R"|| name[0] === "r" ? `${name} plays banjo` : `${name} does not play banjo`}
+
+  //console.log(areYouPlayingBanjo("michael")) 
+  console.log(areYouPlayingBanjo("Risper")) 
