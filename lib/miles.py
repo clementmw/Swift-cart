9c1703ce34ae2bc9a76dc7011d28336c71274fe7
clementmwamba <clementmwamba23@gmail.com>
2023-12-12 14:18:38 +0300

    4

---
+++ lib/miles.py
@@ -0,0 +1,9 @@
+def zero_fuel(distance_to_pump, mpg, fuel_left):
+    miles = mpg * fuel_left
+    if miles >= distance_to_pump: # if the miles is greater or equal to pump then it can reach the pump
+        return True
+    else:
+        return False
+
+print(zero_fuel(34,20,2))
+   
